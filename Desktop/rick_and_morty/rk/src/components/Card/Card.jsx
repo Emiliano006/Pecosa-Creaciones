5df2c113587f7150151f1581005d6198e22815fb
Emiliano006 <emiliano.costabile@hotmail.com>
2023-05-09 23:22:51 -0300

    rickandmorty

---
+++ Desktop/rick_and_morty/rk/src/components/Card/Card.jsx
@@ -0,0 +1,33 @@
+ import style from './Card.module.css'
+ import { Link } from "react-router-dom";
+
+
+
+
+ const Card = ({id, name, status, species, gender, origin, image, onClose}) => {
+  
+   return (
+      <div className='cards'>
+         <Link to={`/detail/${id}`}>
+         <div className='face front'>
+            <img src={image} alt="" />
+         </div>
+         
+         <div className='face back'> 
+             <p>Information:</p>
+             <p>Name: "{name}"</p>
+             <p>Status: "{status}"</p>
+             <p>Species: "{species}"</p>
+             <p>Gender: "{gender}"</p>
+             <p>Origin: "{origin}"</p>
+             <button onClick={()=> {onClose(id)}}>Close</button>
+         </div>
+         </Link>
+         
+      </div>
+   );
+};
+
+export default Card;
+
+
