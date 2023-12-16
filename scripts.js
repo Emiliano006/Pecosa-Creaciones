ce79aaef8af5c782821700483d992a2c366f87a2
Emiliano006 <emiliano.costabile@hotmail.com>
2023-12-15 21:51:24 -0300

    cart

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const carrito = document.getElementById('carrito');
-const elementos1 = document.getElementById('lista1');
+const elementos1 = document.getElementById('lista-1');
 const lista = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
 
